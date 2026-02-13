ba5341c21d2baf2a0ab1b4a344e84f7b762fb667
AlejandroM06 <amontesdom@gmail.com>
2026-02-13 12:27:34 -0600

    new file: fruteria.c

---
+++ fruteria.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float manzanas, uvas, peras, iva;
+    manzanas = 40;
+    uvas = 30;
+    peras = 30;
+    iva = 16 * 0.01;
+
+    printf("FRUTERIA - EL INGE\n");
+    printf("==================");
+    printf("1 kg de manzanas = %.1f", manzanas);
+    
+    return 0;
+}
