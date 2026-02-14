c6e432456c636f4713b0cd6232fe76740aded304
AlejandroM06 <amontesdom@gmail.com>
2026-02-14 15:51:12 -0600

    modified: fruteria.c

--- fruteria.c
+++ fruteria.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
-    float manzanas, uvas, peras, iva;
+    float manzanas, uvas, peras, iva, subtotal, total;
     manzanas = 40;
     uvas = 30;
     peras = 30;
+    subtotal = uvas + peras + manzanas;
     iva = 16 * 0.01;
+    total = (iva * subtotal) + subtotal;
 
     printf("FRUTERIA - EL INGE\n");
-    printf("==================");
-    printf("1 kg de manzanas = %.1f", manzanas);
+    printf("==================\n");
+    printf("1 kg de manzanas = %.1f\n", manzanas);
+    printf("1 kg de uvas = %.1f\n", uvas);
+    printf("1 kg de peras = %.1f\n\n", peras);
+
+    printf("Subtotal = %.2f\n", subtotal);
+    printf("IVA = %.2f\n", iva * subtotal);
+    printf("Total = %.2f\n", total);
     
     return 0;
 }
